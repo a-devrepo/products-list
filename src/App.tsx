cb4edf60abfa521d125afb50575db7fe222cf888
alison <adevpr11@gmail.com>
2023-09-22 18:53:43 -0300

    Adiciona o componente ListingBody

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header"
+import ListingBody from "./components/ListingBody"
 
 function App() {
 
   return (
     <>
       <Header />
+      <ListingBody></ListingBody>
     </>
   )
 }
---
+++ src/components/ListingBody/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+export default function ListingBody() {
+    return (
+        <main>
+            <section id="listing-body-section" className='container'>
+                <h1>Teste</h1>
+            </section>
+        </main>
+    )
+}

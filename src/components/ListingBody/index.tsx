7851a837b929aa4b25a4e93390ddf38d5a00591b
alison <adevpr11@gmail.com>
2023-09-24 15:11:08 -0300

    Inclui chamada ao service para obter dados

--- src/components/ListingBody/index.tsx
+++ src/components/ListingBody/index.tsx
@@ -1,12 +1,35 @@
+import './styles.css';
+import { useEffect, useState } from 'react';
+import { ProductDTO } from '../../models/product';
 import FilterCard from '../FilterCard';
 import ListCard from '../ListCard';
-import './styles.css';
+import * as productService from '../../services/product-service';
+
+
+type QueryParams = {
+    minPrice: number;
+    maxPrice: number;
+}
 
 export default function ListingBody() {
+    4
+
+    const [products, setProducts] = useState<ProductDTO[]>([]);
+    const [queryParams, setQueryParams] = useState<QueryParams>(
+        {
+            minPrice: 0,
+            maxPrice: Number.MAX_VALUE
+        })
 
     function handleSearch(minPrice: number, maxPrice: number) {
-        console.log(minPrice, maxPrice);
+        setQueryParams({ ...queryParams, minPrice, maxPrice });
     }
+
+    useEffect(() => {
+        const list = productService.findByPrice(queryParams.minPrice, queryParams.maxPrice);
+        console.log(list);
+    }, [queryParams])
+
 
     return (
         <main>
